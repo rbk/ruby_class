f9ab114d756eec24cb85f536868d434f9bcf3b62
Richard <Richard.be.jamin@gmail.com>
2013-10-17 23:22:41 -0500

    new assignment

---
+++ RestartMonitor.rb
@@ -0,0 +1,18 @@
+# Create a class called RestartMonitor that 
+# can read from the attached log file and report 
+# the dates and times that system restarts occurred.
+
+# Your RestartMonitor class should have 
+# the ability to work with any specified 
+# log file name, it should be able to search 
+# for system restarts and report on those 
+# restarts independently (i.e. searching 
+# for and showing the restarts do not have 
+# to happen at the same time), and duplicate 
+# restarts should not be shown.
+
+# Restarts can be identified in the log file 
+# by the word "restarting" that will occur in those lines.
+
+
+print ARGV # argument passed from the command line when running script
